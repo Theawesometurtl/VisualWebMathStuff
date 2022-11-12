039472a0ccf80991c4558b07f502ef2947f345dc
Theawesometurtl <shablammmo@gmail.com>
2022-11-12 14:44:39 -0800

    idk its not really what I wanted but I got a basic understanding from it

--- basicCalc.js
+++ basicCalc.js
@@ -3,11 +3,26 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+function theFunction(num) {
+    num = Math.sin(num);
+    return num;
+}
 
 ctx.strokeStyle = 'black';
 ctx.lineWidth = 1;
 for (let i = 0; i < canvas.width; i++) {
-    y = i**2;
-    ctx.fillRect( i, y, 1, 1 );
-    
+    ctx.beginPath();
+    ctx.moveTo(i, -(theFunction(i*.1)*50)+canvas.height-100);
+    ctx.lineTo(i+1, -(theFunction((i+1)*.1)*50)+canvas.height-100);
+    ctx.stroke();
+}
+ctx.strokeStyle = 'green';
+ctx.lineWidth = 1;
+for (let i = 0; i < canvas.width; i++) {
+    prevy = theFunction(i*.1) - theFunction((i-1)*.1);
+    y = theFunction((i+1)*.1) - theFunction(i*.1);
+    ctx.beginPath();
+    ctx.moveTo(i, -(prevy*200)+canvas.height-100);
+    ctx.lineTo(i+1, -(y*200)+canvas.height-100);
+    ctx.stroke();
 }
